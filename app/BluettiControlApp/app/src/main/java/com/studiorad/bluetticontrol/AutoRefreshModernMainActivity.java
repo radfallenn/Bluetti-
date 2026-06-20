30dfd046ea8b0870fe5dd695494018c25700f7a5
radfallenn <113562142+radfallenn@users.noreply.github.com>
2026-06-20 17:49:31 -0300

    Define leitura a cada 5s e atualização visual dos gráficos a cada 30s

--- app/BluettiControlApp/app/src/main/java/com/studiorad/bluetticontrol/AutoRefreshModernMainActivity.java
+++ app/BluettiControlApp/app/src/main/java/com/studiorad/bluetticontrol/AutoRefreshModernMainActivity.java
@@ -3,6 +3,8 @@ package com.studiorad.bluetticontrol;
 import android.os.Bundle;
 
 public class AutoRefreshModernMainActivity extends ModernMainActivity {
+    private long lastVisualChartRefresh = 0;
+
     private final Runnable fiveSecondRefresh = new Runnable() {
         @Override
         public void run() {
@@ -21,6 +23,24 @@ public class AutoRefreshModernMainActivity extends ModernMainActivity {
     @Override
     void readStatus() {
         super.readStatus();
+    }
+
+    @Override
+    void parseRead(byte[] pkt) {
+        long now = System.currentTimeMillis();
+        boolean allowChart = now - lastVisualChartRefresh >= 30000 || lastVisualChartRefresh == 0;
+        if (allowChart) {
+            lastVisualChartRefresh = now;
+            super.parseRead(pkt);
+        } else {
+            int oldBatterySize = batteryHistory.size();
+            int oldConsumptionSize = consumptionHistory.size();
+            super.parseRead(pkt);
+            while (batteryHistory.size() > oldBatterySize) batteryHistory.remove(batteryHistory.size() - 1);
+            while (consumptionHistory.size() > oldConsumptionSize) consumptionHistory.remove(consumptionHistory.size() - 1);
+            if (batteryGraph != null) batteryGraph.setValues(batteryHistory);
+            if (consumptionGraph != null) consumptionGraph.setValues(consumptionHistory);
+        }
     }
 
     @Override
