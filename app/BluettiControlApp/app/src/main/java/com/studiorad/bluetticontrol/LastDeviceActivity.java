7d2bb894c1c0f1d41c69d52a8e358905d3a66b97
radfallenn <113562142+radfallenn@users.noreply.github.com>
2026-06-20 20:44:37 -0300

    Abre configuração para permitir segundo plano

--- app/BluettiControlApp/app/src/main/java/com/studiorad/bluetticontrol/LastDeviceActivity.java
+++ app/BluettiControlApp/app/src/main/java/com/studiorad/bluetticontrol/LastDeviceActivity.java
@@ -1,14 +1,28 @@
 package com.studiorad.bluetticontrol;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
 public class LastDeviceActivity extends AutoRefreshModernMainActivity {
     private boolean triedLast = false;
+    private boolean askedBackground = false;
 
     @Override
     public void onCreate(Bundle b) {
         super.onCreate(b);
+        handler.postDelayed(this::askBackgroundPermission, 1200);
         handler.postDelayed(this::connectLastDevice, 2500);
+    }
+
+    private void askBackgroundPermission() {
+        if (askedBackground) return;
+        askedBackground = true;
+        try {
+            Toast.makeText(this, "Libere o Bluetti Control para rodar em segundo plano", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS));
+        } catch (Exception e) { }
     }
 
     private void connectLastDevice() {
